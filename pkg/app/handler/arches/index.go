3c2f67332467a3f90c3fe3922afcaed5aecc6465
Dennis Lamm <expeditioneer@gentoo.org>
2022-10-09 19:23:34 +0200

    index.go fixed typo

--- pkg/app/handler/arches/index.go
+++ pkg/app/handler/arches/index.go
@@ -5,7 +5,7 @@ import (
 	utils2 "soko/pkg/app/utils"
 )
 
-// Index renders a template to show a the landing page containing links to all arches feeds
+// Index renders a template to show the landing page containing links to all arches feeds
 func Index(w http.ResponseWriter, r *http.Request) {
 	userPreferences := utils2.GetUserPreferences(r)
 	http.Redirect(w, r, "/arches/"+userPreferences.Arches.DefaultArch+"/"+userPreferences.Arches.DefaultPage, http.StatusSeeOther)
